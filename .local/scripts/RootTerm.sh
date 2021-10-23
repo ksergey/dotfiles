084885c95434243b6e1aeba9d978a7369ca7b44d
Sergey Kovalevich <inndie@gmail.com>
2021-10-23 16:25:50 +0300

    Update

---
+++ .local/scripts/RootTerm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CLASSNAME="RootTerm"
+
+WINDOW_ID=$(xdotool search --classname "${CLASSNAME}")
+
+if [ -z "${WINDOW_ID}" ]; then
+  nohup kitty --name=RootTerm &
+else
+  CURRENT_DESKTOP=$(xdotool get_desktop)
+  xdotool set_desktop_for_window ${WINDOW_ID} ${CURRENT_DESKTOP}
+  xdotool windowactivate ${WINDOW_ID}
+  xdotool windowfocus ${WINDOW_ID}
+fi
+
