028ec61be7fb340e23f822710eaaab1dd7961fd1
Sergey Kovalevich <inndie@gmail.com>
2021-10-23 16:32:16 +0300

    update

--- .local/scripts/RootTerm.sh
+++ .local/scripts/RootTerm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# xdotool required
+# kitty, xdotool, tmux
 
 CLASSNAME="RootTerm"
 WINDOW_ID=$(xdotool search --classname "${CLASSNAME}")
 
 if [ -z "${WINDOW_ID}" ]; then
-  nohup kitty --name=RootTerm &
+  nohup kitty --name="${CLASSNAME}" tmux new-session -A -s RootTerm &
 else
   CURRENT_DESKTOP=$(xdotool get_desktop)
   xdotool set_desktop_for_window ${WINDOW_ID} ${CURRENT_DESKTOP}
