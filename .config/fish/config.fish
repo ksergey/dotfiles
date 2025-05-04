bcc4ed10a7cde6b8bb803577bc9388c73beaa52e
Sergey Kovalevich <inndie@gmail.com>
2025-05-04 23:30:24 +0300

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias ls='ls --color=auto'
+    alias grep='grep --color=auto'
+    alias lsblk='lsblk -o name,mountpoint,label,size,uuid'
+    alias ncdu='ncdu --color=dark'
+    alias myip='curl https://ipapi.co/json'
+
+    set -gx EDITOR vim
+    set -gx VISUAL vim
 end
-
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
-alias lsblk='lsblk -o name,mountpoint,label,size,uuid'
-alias ncdu='ncdu --color=dark'
-alias myip='curl https://ipapi.co/json'
-
-set -gx EDITOR vim
-set -gx VISUAL vim
-
-pokemon-colorscripts -r
