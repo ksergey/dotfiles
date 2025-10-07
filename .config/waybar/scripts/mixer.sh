2856fd0b2944376e6083321e94832e4d3bb641f6
Sergey Kovalevich <inndie@gmail.com>
2025-10-07 21:32:07 +0300

    update

--- .config/waybar/scripts/mixer.sh
+++ .config/waybar/scripts/mixer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-hyprctl dispatch exec "kitty --override confirm_os_window_close=0 --class cli-tool -e wiremix"
+kitty --override confirm_os_window_close=0 --class cli-tool -e wiremix
