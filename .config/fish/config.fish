f4b17b9ae470ff271bc177dba9c7547b2a4ddf03
Sergey Kovalevich <inndie@gmail.com>
2025-10-24 23:58:08 +0300

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias ls='ls --color=auto'
-    alias grep='grep --color=auto'
-    alias lsblk='lsblk -o name,mountpoint,label,size,uuid'
-    alias ncdu='ncdu --color=dark'
-    alias myip='curl https://ipapi.co/json'
-
-    set -gx EDITOR vim
-    set -gx VISUAL vim
-
-    starship init fish | source
 end
+
+set -gx EDITOR vim
+set -gx VISUAL vim
+
+if test -f /usr/bin/lsd
+    alias ls "/usr/bin/lsd -l --group-directories-first -h "
+else
+    alias ls "/usr/bin/ls -l --group-directories-first --color=auto -h "
+end
+
+alias grep='grep --color=auto'
+alias lsblk='lsblk -o name,mountpoint,label,size,uuid'
+alias ncdu='ncdu --color=dark'
+alias myip='curl https://ipapi.co/json'
+
+starship init fish | source
---
+++ .config/scripts/mixer.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+kitty --override confirm_os_window_close=0 --class cli-tool -e wiremix
