864efdbcb87b971270079e60514c45fddc914b33
Sergey Kovalevich <inndie@gmail.com>
2025-10-29 01:18:10 +0300

    update

--- .config/scripts/powermenu.sh
+++ .config/scripts/powermenu.sh
@@ -1,38 +1,48 @@
 #!/usr/bin/env bash
 
-# Options
-lock='Lock'
-suspend='Suspend'
-logout='Logout'
-reboot='Reboot'
-shutdown='Shutdown'
+declare -A entries=(
+	[lock]='Lock'
+	[suspend]='Suspend'
+	[logout]='Logout'
+	[reboot]='Reboot'
+	[shutdown]='Shutdown'
+)
+
+declare -A icons=(
+	[lock]=${HOME}/.config/rofi/icons/lock.png
+	[suspend]=${HOME}/.config/rofi/icons/sleep.png
+	[logout]=${HOME}/.config/rofi/icons/logout.png
+	[reboot]=${HOME}/.config/rofi/icons/restart.png
+	[shutdown]=${HOME}/.config/rofi/icons/power.png
+)
+
+for entry in "${!entries[@]}"; do
+	if [ ! -z "${input}" ]; then
+		input="${input}\n"
+	fi
+	input="${input}${entries[$entry]}\0icon\x1f${icons[$entry]}"
+done
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu -theme "$HOME/.config/rofi/powermenu.rasi" -sync
+	rofi -dmenu -theme "$HOME/.config/rofi/powermenu.rasi" -sync -show-icons
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+	echo -e "${input}" | rofi_cmd
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
-	$shutdown)
-		systemctl poweroff
-		;;
-	$reboot)
-		systemctl reboot
-		;;
-	$lock)
+	${entries[lock]})
 		hyprlock
 		;;
-	$suspend)
+	${entries[suspend]})
 		systemctl suspend
 		;;
-	$logout)
+	${entries[logout]})
 		case "${XDG_CURRENT_DESKTOP}" in
 			niri)
 				niri msg action quit
@@ -40,5 +50,11 @@ case ${chosen} in
 			hyprland)
 				hyprctl dispatch exit
 		esac
+		;;
+	${entries[reboot]})
+		systemctl reboot
+		;;
+	${entries[shutdown]})
+		systemctl poweroff
 		;;
 esac
