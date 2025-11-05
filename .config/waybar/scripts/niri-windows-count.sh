280a710c06487184d3eeefd82066a079b5c64294
Sergey Kovalevich <inndie@gmail.com>
2025-11-05 21:50:18 +0300

    update

--- .config/waybar/scripts/niri-windows-count.sh
+++ .config/waybar/scripts/niri-windows-count.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+if ! command -v jq &> /dev/null; then
+  echo "jq not found, make sure it installed"
+  echo "Install jq: yay -S jq"
+  exit 1
+fi
+
 focused_id="$(niri msg -j workspaces | jq ".[] | select(.is_focused == true ) | .id")"
 count="$(niri msg -j windows | jq "[.[] | select(.workspace_id == ${focused_id})] | length")"
 
