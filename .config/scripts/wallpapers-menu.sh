4e9863f0903a2ab787472842f6f3b3ae3ec71195
Sergey Kovalevich <inndie@gmail.com>
2025-11-11 00:38:31 +0300

    update

--- .config/scripts/wallpapers-menu.sh
+++ .config/scripts/wallpapers-menu.sh
@@ -45,7 +45,7 @@ swww img \
   --transition-duration 1 \
   "${wallpaper}"
 
-magick ${wallpaper} -modulate 60,105 -filter Gaussian -resize 20% -blur 0x3.5 "${wallpaper_overview}"
+magick ${wallpaper} -modulate 100,50 -filter Gaussian -resize 20% -blur 0x3.5 "${wallpaper_overview}"
 
 if [ "${XDG_CURRENT_DESKTOP}" == "niri" ]; then
   swww img --namespace overview "${wallpaper_overview}"
