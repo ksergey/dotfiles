dc7b1e08b2e8b564221ed815585e6ed053376b80
Sergey Kovalevich <inndie@gmail.com>
2026-06-10 22:02:28 +0300

    update

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -19,6 +19,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/switch-previous-tab" "<Primary>Page_Up")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/open" "<Primary>o")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-ascending" "")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/e36a3fad55a63ce90ce936f56b86e4bc" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/toggle-split-view" "F3")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/copy-2" "<Primary>Insert")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/trash-delete" "Delete")
--- .config/scripts/power-menu.sh
+++ .config/scripts/power-menu.sh
@@ -2,30 +2,13 @@
 
 all=(Lock Suspend Logout Reboot Shutdown)
 
-declare -A icons=(
-	[Lock]=${HOME}/.config/rofi/icons/lock.png
-	[Suspend]=${HOME}/.config/rofi/icons/sleep.png
-	[Logout]=${HOME}/.config/rofi/icons/logout.png
-	[Reboot]=${HOME}/.config/rofi/icons/restart.png
-	[Shutdown]=${HOME}/.config/rofi/icons/power.png
-)
-
 for entry in ${all[@]}; do
-	icon="${icons[$entry]}"
-	if [ ! -z "${input}" ]; then
-		input="${input}\n"
-	fi
-	input="${input}${entry}\0icon\x1f${icon}"
+	input="${input}${entry}\n"
 done
-
-# Rofi CMD
-rofi_cmd() {
-	rofi -dmenu -theme "$HOME/.config/rofi/power-menu.rasi" -sync -show-icons
-}
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "${input}" | rofi_cmd
+	echo -e "${input}" | rofi -dmenu -theme "$HOME/.config/rofi/power-menu.rasi"
 }
 
 # Actions
@@ -38,13 +21,7 @@ case ${chosen} in
 		systemctl suspend
 		;;
 	Logout)
-		case "${XDG_CURRENT_DESKTOP}" in
-			niri)
-				niri msg action quit
-				;;
-			Hyprland)
-				hyprctl dispatch exit
-		esac
+        loginctl terminate-user "${USER}"
 		;;
 	Reboot)
 		systemctl reboot
